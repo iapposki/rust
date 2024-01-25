8bf0de1db21f4af11c3106419be491cf3f3277e0
iapposki <konark981999@gmail.com>
2024-01-25 15:43:03 +0530

    paths for referring to an item in module tree

---
+++ restaurant-lib-standalone/src/lib.rs
@@ -0,0 +1,59 @@
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        pub fn _seat_at_table() {}
+    }
+
+    mod serving {
+        fn _take_order() {}
+
+        fn _serve_order() {}
+
+        fn _take_payment() {}
+    }
+}
+
+fn _deliver_order() {}
+
+mod back_of_house {
+    fn _fix_incorrect_order() {
+        super::_deliver_order();
+        // user super to access functions in the parent module
+    }
+    fn _cook_order() {}
+    pub struct Breakfast {
+        pub toast: String,
+        _seasonal_fruit: String,
+    }
+    // in case of struct we need to make filed public individually to make them available externally
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                _seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+    // in contrast, to make enum public, we only nee dthe pub before the enum keyword.
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // change our mind about what bread we'd like 
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+    let _order1 = back_of_house::Appetizer::Soup;
+    let _order2 = back_of_house::Appetizer::Salad;
+}
