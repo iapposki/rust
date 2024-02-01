4172aebfa92c79782e2ac5c42bf4cfa361ea5e6d
iapposki <konark981999@gmail.com>
2024-02-01 17:22:42 +0530

    mut vs let

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+
+    // ############# mut vs let #########
+    // let x = 23;
+    // println!("{x}");
+
+    // let x = "some string";
+    // println!("{x}");
+
+    // let mut x = 23;
+    // println!("{x}");
+    // x = 34;
+    // println!("{x}");
+    // x = "34";
+    // println!("{x}");
+
+
+}
