4669e65db65d9623a96c161ac0c2f31c48e35255
iapposki <konark981999@gmail.com>
2024-02-01 17:22:42 +0530

    data types and type conversion

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
 fn main() {
 
     // ############# mut vs let #########
-    // let x = 23;
-    // println!("{x}");
+    /* let x = 23;
+    println!("{x}");
 
-    // let x = "some string";
-    // println!("{x}");
+    let x = "some string";
+    println!("{x}");
 
-    // let mut x = 23;
-    // println!("{x}");
-    // x = 34;
-    // println!("{x}");
-    // x = "34";
-    // println!("{x}");
+    let mut x = 23;
+    println!("{x}");
+    x = 34;
+    println!("{x}");
+    x = "34";
+    println!("{x}"); */
 
 
+    // ########### type conversion and destructuring ############
+/*     let temp : u32 = "47".parse().expect("Not a number");
+    println!("{temp}");
+
+    let tup = (500, 6.4, 1);
+    let (_x, y, _z) = tup;
+    println!("The value of y is: {y}");
+
+    let _arr1 = [3,4,5,5];
+    let _arr2 = [2.3,4.0,3.3];
+    let _arr3 = [3;4]; // same as [3,3,3,3]
+    // array are immutable */
 }
