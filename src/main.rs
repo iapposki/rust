1fdcf778a0dc906d5d7d0e6fe1ff2b2fdaa22706
iapposki <konark981999@gmail.com>
2024-04-29 15:17:41 +0530

    panic

--- src/main.rs
+++ src/main.rs
@@ -578,9 +578,25 @@ fn main() {
         
     }
 
-    // ##################  ####################
+    // ################## panic ####################
     {
-
+        #[derive(Debug)]
+        struct Guess {
+            value: i32,
+        }
+        impl  Guess {
+            fn new (value: i32) -> Guess {
+                if value < 1 || value > 100 {
+                    panic!("Guess value must be between 1 and 100, got {}", value);
+                }
+                Guess {value}
+            }
+            fn value (&self) -> i32 {
+                self.value
+            }
+        }
+        let guess = Guess::new(19);
+        println!("{:?}, {}", guess, guess.value());
     }
 
     let elapsed_time = start_time.elapsed();
