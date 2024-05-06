84a63d064273ee73866ee05b5c174120dc35db8a
iapposki <konark981999@gmail.com>
2024-05-06 17:26:01 +0530

    validating references with lifetimes and generic lifetimes in funcitons; their syntaxes and signatures

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,12 @@ use core::panic;
 use std::collections::HashMap;
 use std::fmt::{Debug, Display};
 use std::io::{self, ErrorKind, Read};
-use std::str::FromStr;
 use std::time::Instant;
 use std::fs::{self, File};
 
 use crate::garden::vegetables::Asparagus;
 use rusttutorial::{front_of_house, Summary};
 use rusttutorial::Tweet;
-use rusttutorial::NewsArticle;
 
 
 pub mod garden;
@@ -715,6 +713,7 @@ fn main() {
             return 0
         }
     }
+
     // ################# returning types that implement traits ##############
     fn _returns_summarizable() -> impl Summary {
         Tweet {
@@ -738,6 +737,36 @@ fn main() {
     let number_list = vec![23,43,45,545643];
     let result = largest(&number_list);
     println!("{result}");
+
+    // ######################### Validating references with lifetimes ############################
+    // consider function:
+    // fn longest(x: &str, y: &str) -> &str {
+    //     if x.len() > y.len() {
+    //         x
+    //     } else {
+    //         y
+    //     }
+    // }
+    // this gives an error saying the return type needs a generic lifetime parameter on it because rust cant tell wheather the reference being returned refers to x or y.
+    // examples of lifetime parameter:
+    // &i32   // a reference
+    // &'a i32   // a reference with an explicit lifetime
+    // &'a mut i32  // a mutable reference with an explicit lifetime
+    // to use these in functions:
+    fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+        if x.len() > y.len() {
+            x
+        } else {
+            y
+        }
+    }
+    // The function signature now tells Rust that for some lifetime 'a, the function takes two parameters, both of which are string slices that live at least as long as lifetime 'a. The function signature also tells Rust that the string slice returned from the function will live at least as long as lifetime 'a. In practice, it means that the lifetime of the reference returned by the longest function is the same as the smaller of the lifetimes of the values referred to by the function arguments. These relationships are what we want Rust to use when analyzing this code.
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+    
+
 
 
     let elapsed_time = start_time.elapsed();
